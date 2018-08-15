a86128968bbef79470fee0eb3a1e933a7f9203ce
GarimaSingh1997 <42217662+GarimaSingh1997@users.noreply.github.com>
2018-08-15 22:20:36 +0530

    quotient_remainder
    print quotient and remainder from 2 number

---
+++ quotient-remainder.py
@@ -0,0 +1,6 @@
+m=int(input("Enter the first number: "))
+n=int(input("Enter the second number: "))
+quotient=m//n
+remainder=m%n
+print("quotient of number ",quotient)
+print("remainder of number ",remainder)
