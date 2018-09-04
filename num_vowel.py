38044864a345f78c7417d7186aea31e09bf6e4fb
GarimaSingh1997 <42217662+GarimaSingh1997@users.noreply.github.com>
2018-09-04 23:50:36 +0530

    find number of vowels in a string

---
+++ num_vowel.py
@@ -0,0 +1,9 @@
+ip_str = input("Enter a string: ")
+
+# make it suitable for caseless comparisions
+ip_str = ip_str.casefold()
+
+# count the vowels
+count = {x:sum([1 for char in ip_str if char == x]) for x in 'aeiou'}
+
+print(count)
